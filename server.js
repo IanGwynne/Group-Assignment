4505be00e32ebf594e90f1b805454533f296105f
Marco <113130222+MarcoDeMelo@users.noreply.github.com>
2022-11-28 16:09:25 -0500

    Made a Few Changes to the website. Finishing stuff

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 //Group 5 
 //Kian Petroskie, Marco DeMelo, Ian Haworth
 let express = require('express');
+const { getCon } = require('./mysql/config');
 let app = express();
-
+const mysql = require('mysql');
 app.listen(3000);
 
 app.get("/", (request, response) => {
-  response.render('index.ejs');
+ let con= getCon();
+
+ con.connect();
+  let query 
+  = `SELECT * FROM restaurants `;
+//‘${request.query.filter}’` Maybe?
+  let safeQuery = mysql.format(query,);
+
+  con.query(safeQuery, (error, result) => {
+      response.render('index.ejs',
+      {
+          results:result
+      });
+  });
+   
+  con.end();
 });
