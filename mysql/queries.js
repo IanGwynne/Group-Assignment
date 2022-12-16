f404e54a6b172993fe1f7364ad050aed6a80be07
kianpetroskie <kian.petroskie@student.sl.on.ca>
2022-12-16 18:37:42 -0500

    hardcoded filter

--- mysql/queries.js
+++ mysql/queries.js
@@ -2,9 +2,9 @@ const mysql = require("./config.js");
 
 function getFromRestaurants()
 {
-    let query = `SELECT * FROM restaurants `;
-    let safeQuery = mysql.functions.format(query)
+    let query = `SELECT * FROM restaurants`;
 
+    let safeQuery = mysql.functions.format(query)
     return querySql(safeQuery);
 }
 
@@ -12,6 +12,19 @@ function getTableInfo(criteria)
 {
     let query = `SELECT name, city, country, cuisine FROM restaurants
      ORDER BY name LIMIT ?,?`;
+
+    let checkedFilter = 4 //change value to whichever option is chosen by user (city country or quisine)
+
+   if (checkedFilter==2)
+       query = `SELECT name, city, country, cuisine FROM restaurants WHERE country = 'Italy' 
+       ORDER BY name LIMIT ?,?`; // change hardcoded value of Italy to what the user chose from 2nd dropbox
+   else if (checkedFilter==3)
+       query = `SELECT name, city, country, cuisine FROM restaurants WHERE city = 'Santa Rita'
+       ORDER BY name LIMIT ?,?`; // change hardcoded value of Santa Rita to what the user chose from 2nd dropbox
+   else if (checkedFilter==4)
+       query = `SELECT name, city, country, cuisine FROM restaurants WHERE cuisine = 'Italian'
+       ORDER BY name LIMIT ?,?`; // change hardcoded value of Italian to what the user chose from 2nd dropbox
+
     let safeQuery = mysql.functions.format(query, [criteria.skip, criteria.take]);
     return querySql(safeQuery);
 }
