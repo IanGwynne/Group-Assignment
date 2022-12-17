4d6ee180640592c4e211cc7bc4a0b7062b9d97bf
kianpetroskie <kian.petroskie@student.sl.on.ca>
2022-12-16 23:08:07 -0500

    deleted uneccessary comments

--- public/functions.js
+++ public/functions.js
@@ -1,10 +1,7 @@
-
-
     let table = document.querySelector('table tbody');
     let pageNumber = 0;
     let take = 5;
     let firstBox="none",secondBox="0";
-    //Need to figure out how to do this with ejs i guess.
     
     function dropBoxChange()
     {
@@ -53,21 +50,17 @@
     getData();
     dropBoxChange();
     document.querySelector("#previous").addEventListener("click", event => {
-        // this needs to decrement the page number and re-fetch the data
         if(pageNumber != 0)
           pageNumber--;
         getData();
     });
 
     document.querySelector("#next").addEventListener("click", event => {
-        // this needs to increment the page number and re-fetch the data
         pageNumber++;
         getData();
     });
 
     document.querySelector("#tableList").addEventListener("change", event => {
-        // reset pageNumber to 0 to make it easier to work with
-        // this needs to update the take and re-fetch the data
       pageNumber = 0;
       let perPage = document.querySelector('#tableList option:checked').value
       take = perPage;
@@ -77,8 +70,6 @@
 
     document.querySelector('#apply').addEventListener("click", event => {
         pageNumber = 0;
-      // needs to grab the currently selected thing from the second filter box
-      //firstBox = document.querySelector('#filterList option:checked').value;
       secondBox = document.querySelector('#List option:checked').value;
       getData();
       document.getElementById("clear").disabled = false;
